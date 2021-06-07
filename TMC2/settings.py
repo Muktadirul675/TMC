7f4c0e5986eff230c3b0f7bef0fcb35a5e0204d1
Muktadirul675 <muktadirul.05@gmail.com>
2021-06-07 13:48:14 +0600

    C

--- TMC2/settings.py
+++ TMC2/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'vxnd10se+eqe!r7k=ep&9ixakm##=otnyfo1!=q=1=bz@le0@y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
